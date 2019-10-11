5bf534fbfbbf70cb5ca0a206b05d8bdd078a9d46
Tricktionary <andyaoux@hotmail.com>
2019-10-11 09:30:35 -0400

    Stable

--- spec/graphql/mutations/update_song_spec.rb
+++ spec/graphql/mutations/update_song_spec.rb
@@ -7,14 +7,14 @@ describe Mutations::UpdateSong do
 
   describe 'mutation' do
 
-    context 'Update song' do
+    context 'when name is changed' do
       let!(:song_a) { create(:song, name: 'testA', page_range_end: 2, page_range_end: 10) }
-      it 'name is changed' do  
+      it 'name is valid' do  
         result = resolver.resolve(id:song_a.id ,name: 'Are you bored yet?')
         expect(result[:song].name).to eq('Are you bored yet?')
       end
       
-      it 'has invalid name not changed' do  
+      it 'name is invalid' do  
         result = resolver.resolve(id:song_a.id ,name: '')
         expect(result[:song].name).to eq('testA')
       end
